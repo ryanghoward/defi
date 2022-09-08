b7ab83c6b28aa849ad8679073f0e595e5ed177da
Ryan Howard <ryanghoward@protonmail.com>
2022-09-08 14:59:55 -0700

    Hero section done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,31 @@ const Hero = () => {
   return (
     <div className='w-full h-[90vh] top-[90px]'>
       <video
-        className='object-cover h-full w-full absolute'
+        className='object-cover h-full w-full absolute z-[-1]'
         src={video}
         autoPlay
         loop
         muted
       />
+      <div className='w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center'>
+        <h1>Decentralized</h1>
+        <h1 className='py-2'>
+          <span className='blue'>Trading</span> Protocol
+        </h1>
+        <p className='text-xl py-4'>
+          Guaranteed liquidity trading for millions of users and top Ethereum
+          applications
+        </p>
+        <div>
+          <button className='m-2'>Use Defi</button>
+          <button className='m-2'>FAQ</button>
+        </div>
+      </div>
+      <div>
+        <p className='text-center text-white text-2xl font-bold'>
+          Total Volume Secured: $69,420,696,420.69
+        </p>
+      </div>
     </div>
   );
 };
